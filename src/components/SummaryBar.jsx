85dcd092b0257639a40578e07c72c291e002b2d6
Trisha Marie Estrecho <tmestrecho2024@hccci.edu.ph>
2026-09-05 13:14:08 +0800

    Update SummaryBar.jsx

--- src/components/SummaryBar.jsx
+++ src/components/SummaryBar.jsx
@@ -1,6 +1,15 @@
 export default function SummaryBar({ total, favorites }) {
   return (
     <div className="stats shadow w-full">
+      <div className="stats shadow w-full">
+        <div className="stat-title">total recipes</div>
+        <div className="stat-value">{total}</div>
+      </div>
+
+      <div className="stat">
+        <div className="stat-title">Favorites</div>
+        <div className="stat-value">{favorites} ★</div>
+      </div>
       {/* TODO: two <div className="stat"> blocks.
           - First: title "Total Recipes", value {total}
           - Second: title "Favorites", value {favorites} ★
