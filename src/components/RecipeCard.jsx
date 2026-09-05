0689da7e4f2c56d7115ecc4c61540977a0ea7e71
Trisha Marie Estrecho <tmestrecho2024@hccci.edu.ph>
2026-09-05 13:11:32 +0800

    Update RecipeCard.jsx

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -6,19 +6,35 @@ export default function RecipeCard({ recipe, onToggleFavorite, onDelete }) {
       <div className="card-body">
         <div className="flex justify-between items-start">
           {/* TODO: show the recipe title as an <h2 className="card-title"> */}
+          <h2 className="card-title">{title}</h2>
 
           {/* TODO: favorite toggle button.
               - Shows ★ when favorite is true, ☆ when false.
               - onClick calls onToggleFavorite(id). */}
+              <button
+              className="btn btn-ghost btn-sm text-2x1"
+              onClick={() => onToggleFavorite(id)}
+              aria-label="Togglr Favorite"
+              >
+                { favorite ? "★" : "☆"}
+              </button>
         </div>
 
         <div className="flex gap-2 mt-1">
           {/* TODO: category badge (badge badge-primary) */}
+          <span className="badge badge-primary">{category}</span>
           {/* TODO: time badge (badge badge-ghost) — e.g., "45 min" */}
+          <span className="badge badge-ghost">{time} min</span>
         </div>
 
         <div className="card-actions justify-end mt-3">
-          {/* TODO: Delete button (btn btn-error btn-sm) that calls onDelete(id) */}
+           {/* TODO: Delete button (btn btn-error btn-sm) that calls onDelete(id) */}
+          <button
+          className="btn btn-error tbtn-sm"
+          onClick={() => onDelete(id)}
+          >
+            Delete
+          </button>
         </div>
       </div>
     </div>
